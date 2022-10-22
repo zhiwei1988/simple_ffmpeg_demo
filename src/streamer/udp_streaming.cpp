5a52ed2b237b752ba94bcd907ee7070a9db89843
zhiwei1988 <zhiweix1988@gmail.com>
2022-10-22 17:43:38 +0800

    bugfix

--- src/streamer/udp_streaming.cpp
+++ src/streamer/udp_streaming.cpp
@@ -2,7 +2,6 @@
 * 使用 rtp over udp 对 h264 文件进行串流
 */
 
-
 #include <stdio.h>
 
 #ifdef __cplusplus
@@ -17,37 +16,36 @@ extern "C"
 #endif
 
 int main(int argc, char* argv[])
-{    
+{
     const char* in_filename = "outdoor.h264";
-    const char* out_filename = "udp://192.168.200.1:1234";
+    const char* out_filename = "rtp://192.168.200.1:1234";
 
     // 打开输入
     AVFormatContext *ifmt_ctx = nullptr;
-    int32_t ret = avformat_open_input(&ifmt_ctx, in_filename, 0, 0);
+    int32_t video_index = -1;
+    AVFormatContext *ofmt_ctx = nullptr;
+    AVOutputFormat *ofmt = nullptr;
+    int64_t start_time = av_gettime();
+    int32_t frame_index = 0;
+
+    int32_t ret = avformat_open_input(&ifmt_ctx, in_filename, nullptr, nullptr);
     if (ret < 0) {
         printf( "Could not open input file.\n");
         goto end;
     }
 
     // 查找输入流信息
-    ret = avformat_find_stream_info(ifmt_ctx, 0);
+    ret = avformat_find_stream_info(ifmt_ctx, nullptr);
     if (ret < 0) {
         printf( "Failed to retrieve input stream information\n");
         goto end;
     }
 
-    int32_t video_index = -1;
-    for(int32_t i = 0; i < ifmt_ctx->nb_streams; i++) {
-        if(ifmt_ctx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO) {
-            video_index = i;
-            break;
-        }
-    }
+    video_index = av_find_best_stream(ifmt_ctx, AVMEDIA_TYPE_VIDEO, -1, -1, nullptr, 0);
 
     av_dump_format(ifmt_ctx, 0, in_filename, 0);
 
     // 输出
-    AVFormatContext *ofmt_ctx = nullptr;
     avformat_alloc_output_context2(&ofmt_ctx, nullptr, "rtp", out_filename);
     if (ofmt_ctx == nullptr) {
         printf( "Could not create output context\n");
@@ -55,15 +53,15 @@ int main(int argc, char* argv[])
         goto end;
     }
 
-    AVOutputFormat *ofmt = ofmt_ctx->oformat;
-    for (i = 0; i < ifmt_ctx->nb_streams; i++) {
+    ofmt = ofmt_ctx->oformat;
+    for (int32_t i = 0; i < ifmt_ctx->nb_streams; i++) {
         // 根据输入流创建输出流，这里我们只处理视频流
         if (i != video_index) {
             continue;
         }
 
         AVStream *in_stream = ifmt_ctx->streams[i];
-        AVStream *out_stream = avformat_new_stream(ofmt_ctx, in_stream->codec->codec);
+        AVStream *out_stream = avformat_new_stream(ofmt_ctx, nullptr);
         if (out_stream == nullptr) {
             printf( "Failed allocating output stream\n");
             ret = AVERROR_UNKNOWN;
@@ -71,7 +69,7 @@ int main(int argc, char* argv[])
         }
 
         // 复制 AVCodecContext 的设置
-        ret = avcodec_copy_context(out_stream->codec, in_stream->codec);
+        ret = avcodec_parameters_copy(out_stream->codecpar, in_stream->codecpar);
         if (ret < 0) {
             printf( "Failed to copy context from input to output stream codec context\n");
             goto end;
@@ -104,68 +102,63 @@ int main(int argc, char* argv[])
         goto end;
     }
 
-    int64_t start_time = av_gettime();
-    int32_t frame_index=0;
     while (1) {
-        
-        AVStream *out_stream = nullptr;
-
         // 获取一个AVPacket
-        AVPacket pkt;
-        ret = av_read_frame(ifmt_ctx, &pkt);
+        AVPacket *pkt = av_packet_alloc();
+        ret = av_read_frame(ifmt_ctx, pkt);
         if (ret < 0) {
             break;
         }
 
-        if (pkt.stream_index != video_index) {
+        if (pkt->stream_index != video_index) {
             continue;
         }
-            
+
         // FIX：No PTS (Example: Raw H.264)
         // Simple Write PTS
-        if(pkt.pts == AV_NOPTS_VALUE){
+        if(pkt->pts == AV_NOPTS_VALUE){
             //Write PTS
             AVRational time_base = ifmt_ctx->streams[video_index]->time_base;
-            
+
             // Duration between 2 frames (us)
             // 根据帧计算两帧之间的间隔
             int64_t calc_duration = (double)AV_TIME_BASE / av_q2d(ifmt_ctx->streams[video_index]->r_frame_rate);
 
             //Parameters
             // 以下计算值都是以输入流的 time_base 为基准
-            pkt.pts = (double)(frame_index * calc_duration) / (double)(av_q2d(time_base) * AV_TIME_BASE);
-            pkt.dts = pkt.pts;
-            pkt.duration = (double)calc_duration / (double)(av_q2d(time_base) * AV_TIME_BASE);
+            pkt->pts = (double)(frame_index * calc_duration) / (double)(av_q2d(time_base) * AV_TIME_BASE);
+            pkt->dts = pkt->pts;
+            pkt->duration = (double)calc_duration / (double)(av_q2d(time_base) * AV_TIME_BASE);
         }
 
         //Important:Delay 保证按帧率进行发送
         AVRational time_base = ifmt_ctx->streams[video_index]->time_base;
         AVRational time_base_q = {1, AV_TIME_BASE};
-        int64_t pts_time = av_rescale_q(pkt.dts, time_base, time_base_q);
+        int64_t pts_time = av_rescale_q(pkt->dts, time_base, time_base_q);
         int64_t now_time = av_gettime() - start_time;
         if (pts_time > now_time) {
             av_usleep(pts_time - now_time);
-        }   
+        }
 
-        AVStream *in_stream  = ifmt_ctx->streams[pkt.stream_index];
-        AVStream *out_stream = ofmt_ctx->streams[pkt.stream_index];
+        AVStream *in_stream  = ifmt_ctx->streams[pkt->stream_index];
+        AVStream *out_stream = ofmt_ctx->streams[pkt->stream_index];
 
         // 转换PTS/DTS
-        pkt.pts = av_rescale_q_rnd(pkt.pts, in_stream->time_base, out_stream->time_base, (AVRounding)(AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
-        pkt.dts = av_rescale_q_rnd(pkt.dts, in_stream->time_base, out_stream->time_base, (AVRounding)(AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
-        pkt.duration = av_rescale_q(pkt.duration, in_stream->time_base, out_stream->time_base);
-        pkt.pos = -1;
+        pkt->pts = av_rescale_q_rnd(pkt->pts, in_stream->time_base, out_stream->time_base, (AVRounding)(AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
+        pkt->dts = av_rescale_q_rnd(pkt->dts, in_stream->time_base, out_stream->time_base, (AVRounding)(AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
+        pkt->duration = av_rescale_q(pkt->duration, in_stream->time_base, out_stream->time_base);
+        pkt->pos = -1;
 
-        printf("Send %8d video frames to output URL\n",frame_index);
+        printf("Send %8d video frames to output URL\n", frame_index);
         frame_index++;
-        
-        ret = av_interleaved_write_frame(ofmt_ctx, &pkt);
+
+        ret = av_interleaved_write_frame(ofmt_ctx, pkt);
         if (ret < 0) {
             printf( "Error muxing packet\n");
             break;
         }
-        
-        av_free_packet(&pkt);
+
+        av_packet_unref(pkt);
     }
 
     // 写文件尾
@@ -184,6 +177,6 @@ end:
         printf( "Error occurred.\n");
         return -1;
     }
-    
+
     return 0;
 }
